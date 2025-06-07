c0dfa67330f38173266b7586863edd4e26773333
shaqui-12 <ae0327205@gmail.com>
2025-06-06 22:05:06 -0600

    Add files via upload

---
+++ tiempoejec.cpp
@@ -0,0 +1,27 @@
+
+#include <stdio.h>
+#include <time.h>
+int main() {
+    clock_t start, end;
+    double tiempo;
+
+    start = clock();
+
+    int x = 17, y;
+    int *p;
+    p = &x;
+
+    printf("El valor de x es %d", *p);
+    y = *p + 3;
+    printf("\nEl valor de y es %d", y);
+
+    end = clock();
+    tiempo = (double)(end - start) * 1000 / CLOCKS_PER_SEC; 
+    printf("\nTiempo de ejecucion con apuntador: %.3f ms\n", tiempo);
+
+    return 0;
+}
+
+
+
+
